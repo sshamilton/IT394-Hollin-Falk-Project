1dc9441bbab865c2ddb4f222de4d5e7b828a688c
cjfalk32 <59667387+cjfalk32@users.noreply.github.com>
2020-04-27 14:36:30 -0400

    first
    first

--- mysite/codhc/models.py
+++ mysite/codhc/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
