64dddae97658964603428a0305a33e8a99f8cabe
opiwv <orion.hollin@westpoint.edu>
2020-05-03 16:00:59 -0400

    models / views on codhc
    in progress of establishing models / views for codhc app based upon the polls app generated in class

--- mysite/codhc/models.py
+++ mysite/codhc/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
     title = models.CharField(max_length=100)
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,6 +13,7 @@ class Post(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     
--- mysite/codhc/views.py
+++ mysite/codhc/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Post, Comment
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
-
+    latest_post_list = Post.objects.filter(pub_date__lte=timezone.now()).order_by('-created_at')[:5]
+    context = {'latest_post_list': latest_post_list}
+    return render(request, 'codhc/index.html', context)
